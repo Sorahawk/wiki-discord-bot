5e7eae5ae73f25f683e06a1225764f62ee5d2797
Sorahawk <sorahawktech@gmail.com>
2026-09-05 02:09:14 +0800

    shorten interval

--- python-scripts/var_global.py
+++ python-scripts/var_global.py
@@ -51,7 +51,7 @@ MAX_QUERY_TITLES = 500
 ### SYNC ###
 
 # interval (seconds) between periodic runs of sync task
-SYNC_INTERVAL_SECONDS = 20
+SYNC_INTERVAL_SECONDS = 15
 
 # boolean flag used to denote wiki state as changed; flipped when Mentat messages are detected in wiki feed channel
 WIKI_CHANGED_FLAG = False

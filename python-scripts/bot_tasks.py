0433d67a21d6d49417eead67f6b9adb2eb13fa9f
Sorahawk <sorahawktech@gmail.com>
2026-09-26 21:35:25 +0800

    test the protection task

--- python-scripts/bot_tasks.py
+++ python-scripts/bot_tasks.py
@@ -61,7 +61,7 @@ class TasksCog(commands.Cog):
 
 
 	# verify page protection
-	@loop(hours=1)
+	@loop(minutes=1)
 	async def task_protect_pages(self):
 		if sys.platform != 'linux' or var_global.SLEEP_MODE:
 			return
